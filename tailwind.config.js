c540db9299761cbde6b12eef2416edbbcbb56a22
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-01-23 02:39:39 +0600

    completed program sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('./Images/2.png')",
+          'hero-pattern': "url('./Images/3.png')",
+          'hero-pattern': "url('./Images/4.png.png')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      },
     },
     plugins: [],
   }
